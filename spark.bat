6d41a4e07d8868770b80d7ebc345cc31380f53c5
Jáder Oliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-10-03 15:34:07 -0300

    Initial commit

---
+++ spark.bat
@@ -0,0 +1,7 @@
+@echo Script para inicializacao do Serve Spark
+@echo Criado por Jader Oliveira
+@echo Para parar o Spark Serve pressione a tecla CONTROL + C
+@echo Em seguida Digite S e pressione ENTER
+@echo off
+pause
+php spark serve
